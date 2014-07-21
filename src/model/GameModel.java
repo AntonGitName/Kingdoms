bcb9b79b2b9d89cad3a5b22268f47453794d67d0
Anton Mordberg <mordbergak@gmail.com>
2014-07-21 16:53:57 +0200

    minor changes

--- src/model/GameModel.java
+++ src/model/GameModel.java
@@ -1,17 +1,31 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GameModel {
 	
 	private final GameField field;
 	
 	private int numberOfTeams;
 	
+	private List<Move> moves = new ArrayList<>();
+	
 	public GameModel(int width, int height) {
 		field = new GameField(width, height);
 		numberOfTeams = 2;
 	}
 	
-	public void makeTurn() {
+	public void addMove(int fromX, int fromY, int toX, int toY) {
 		
+		// various checks here
+		
+		moves.add(new Move(fromX, fromY, toX, toY));
+	}
+	
+	public void makeTurn() {
+		for (Move move : moves) {
+			field.makeMove(move);
+		}
 	}
 }
