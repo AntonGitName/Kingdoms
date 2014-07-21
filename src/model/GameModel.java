22201f8c4048c0f4b6c35888bf7ebb5442600277
Anton Mordberg <mordbergak@gmail.com>
2014-07-21 17:00:32 +0200

    minor changes

--- src/model/GameModel.java
+++ src/model/GameModel.java
@@ -9,23 +9,25 @@ public class GameModel {
 	
 	private int numberOfTeams;
 	
-	private List<Move> moves = new ArrayList<>();
+	private int currentTeamTurn;
 	
 	public GameModel(int width, int height) {
 		field = new GameField(width, height);
 		numberOfTeams = 2;
+		currentTeamTurn = 0;
 	}
 	
-	public void addMove(int fromX, int fromY, int toX, int toY) {
+	public void makeMove(int fromX, int fromY, int toX, int toY) {
+		
+		Move move = new Move(fromX, fromY, toX, toY);
 		
 		// various checks here
 		
-		moves.add(new Move(fromX, fromY, toX, toY));
 	}
 	
-	public void makeTurn() {
-		for (Move move : moves) {
-			field.makeMove(move);
-		}
+	public void endTurn() {
+		
+		currentTeamTurn = (currentTeamTurn + 1) % numberOfTeams;
+		
 	}
 }
